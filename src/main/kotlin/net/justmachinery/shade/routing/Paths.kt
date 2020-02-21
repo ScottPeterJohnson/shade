60138e783a759e9ec6cd246363ba860454249396
scott <scott@justmachinery.net>
2020-02-20 23:45:26 -0800

    Add a utility function to UrlInfo

--- src/main/kotlin/net/justmachinery/shade/routing/Paths.kt
+++ src/main/kotlin/net/justmachinery/shade/routing/Paths.kt
@@ -3,6 +3,7 @@ package net.justmachinery.shade.routing
 import net.justmachinery.shade.ObservableValue
 import net.justmachinery.shade.mergeMut
 import org.apache.http.client.utils.URLEncodedUtils
+import org.apache.http.message.BasicNameValuePair
 import java.nio.charset.Charset
 
 class PathData {
@@ -50,18 +51,34 @@ class PathData {
 interface UrlInfo {
     val pathSegments : Sequence<String>
     val queryParams : Sequence<Pair<String,String>>
+    val pathInfo : String
+    val queryString : String
 
     companion object {
-        fun of(pathInfo : String?, queryString : String?) : UrlInfo = ParseUrlInfo(pathInfo, queryString)
+        fun of(pathInfo : String?, queryString : String?) : UrlInfo = ParseUrlInfo(pathInfo ?: "", queryString ?: "")
+    }
+
+    fun removePathPrefix(prefix : String) : UrlInfo {
+        val inf = pathInfo
+        return if(inf.length > prefix.length && inf[prefix.length] != '/'){
+            this
+        } else {
+            ParseUrlInfo(pathInfo.removePrefix(prefix), queryString)
+        }
     }
 }
 
 internal class BasicUrlInfo(
     override val pathSegments: Sequence<String>,
     override val queryParams: Sequence<Pair<String, String>>
-) : UrlInfo
+) : UrlInfo {
+    override val pathInfo: String
+        get() = pathSegments.joinToString("/")
+    override val queryString: String
+        get() = URLEncodedUtils.format(queryParams.map { BasicNameValuePair(it.first, it.second) }.asIterable(), Charset.forName("ASCII"))
+}
 
-private class ParseUrlInfo(pathInfo : String?, queryString : String?) : UrlInfo {
-    override val pathSegments by lazy { URLEncodedUtils.parsePathSegments(pathInfo ?: "").asSequence() }
-    override val queryParams by lazy { URLEncodedUtils.parse(queryString, Charset.defaultCharset()).asSequence().map { it.name to it.value } }
+private class ParseUrlInfo(override val pathInfo : String, override val queryString : String) : UrlInfo {
+    override val pathSegments by lazy { URLEncodedUtils.parsePathSegments(pathInfo).asSequence() }
+    override val queryParams by lazy { URLEncodedUtils.parse(queryString, Charset.forName("ASCII")).asSequence().map { it.name to it.value } }
 }
